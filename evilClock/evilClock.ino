fca26cdc749c5c42e7b5799b4382fbb27df75869
Paweł Zawistowski <pawel.zawistowski@pw.edu.pl>
2024-04-14 18:40:31 +0200

    Cleanup cont

--- evilClock/evilClock.ino
+++ evilClock/evilClock.ino
@@ -1,4 +1,4 @@
-#include "M5StickCPlus2.h"
+#include <M5StickCPlus2.h>
 #include <TFT_eSPI.h>
 #include <ESP32Time.h>
 #include <EEPROM.h>
@@ -11,6 +11,7 @@ TFT_eSprite sprite = TFT_eSprite(&tft);
 #include "bigFont.h"
 #include "secFont.h"
 #include "Arduino.h"
+#include "ppg.h"
 
 // main.h - macros
 #define LED 36     // LED indicator pin (built-in LED)
@@ -48,6 +49,7 @@ uint8_t bits_r = 0;
 uint8_t code_ptr;
 
 ESP32Time rtc(0);
+long timerStartEpoch = rtc.getEpoch();
 #define EEPROM_SIZE 4
 
 // colors
@@ -113,9 +115,6 @@ bool sendIR = false;
 
 // stopwatch variable
 int fase = 0; // 0=0 1=start 2=stop
-String minn = "00";
-String secc = "00";
-String mill = "00";
 
 // sleep variables
 int sleepTime = 10;
@@ -136,25 +135,24 @@ void setup() // ##############  SETUP ##################
   auto cfg = M5.config();
   StickCP2.begin(cfg);
   // StickCP2.Rtc.setDateTime( { { 2024, 2, 17 }, { 8, 20, 0 } } );
-
+  timerStartEpoch = rtc.getEpoch();
   pinMode(LED, OUTPUT);
   pinMode(TRIGGER, INPUT_PULLUP);
 
   delay_ten_us(5000);
 
-  rtc.setTime(0, 0, 0, 1, 1, 2023, 0);
   EEPROM.begin(EEPROM_SIZE);
   b = EEPROM.read(0);
   if (b > 7)
     b = 2;
 
-  buzzer = EEPROM.read(1);
-  if (buzzer > 1)
-    buzzer = 0;
+  // buzzer = EEPROM.read(1);
+  // if (buzzer > 1)
+    // buzzer = 0;
 
   StickCP2.Display.setBrightness(brightnes[b]);
 
-  sprite.createSprite(135, 240);
+  sprite.createSprite(240, 135);
   sprite.setSwapBytes(true);
 
   for (int i = 0; i < 8; i++)
@@ -199,180 +197,209 @@ void drawMain()
   sprite.fillSprite(TFT_BLACK);
   sprite.setTextDatum(4);
 
-  sprite.fillSmoothRoundRect(6, 6, 98, 124, 2, grays[7], TFT_BLACK);
-  sprite.fillSmoothRoundRect(8, 8, 94, 120, 2, TFT_BLACK, grays[7]);
+  
+  // sprite.fillSmoothRoundRect(46, 158, 84, 75, 2, grays[7], TFT_BLACK);
+  // sprite.fillSmoothRoundRect(48, 160, 80, 71, 2, TFT_BLACK, grays[7]);
+  // sprite.fillSmoothRoundRect(52, 209, 72, 18, 2, blue2, TFT_BLACK);
+  // sprite.fillSmoothRoundRect(6, 214, 34, 18, 2, TFT_ORANGE, TFT_BLACK);
 
-  sprite.fillSmoothRoundRect(46, 158, 84, 75, 2, grays[7], TFT_BLACK);
-  sprite.fillSmoothRoundRect(48, 160, 80, 71, 2, TFT_BLACK, grays[7]);
-  sprite.fillSmoothRoundRect(52, 209, 72, 18, 2, blue2, TFT_BLACK);
-  sprite.fillSmoothRoundRect(6, 214, 34, 18, 2, TFT_ORANGE, TFT_BLACK);
+  // // seconds %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  // sprite.loadFont(secFont);
+  // sprite.setTextColor(grays[5], TFT_BLACK);
 
-  // seconds %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  sprite.loadFont(secFont);
-  sprite.setTextColor(grays[5], TFT_BLACK);
+  // for (int i = 0; i < 60; i++)
+  //   if (i % 5 == 0)
+  //     sprite.fillSmoothCircle(px3[i], py3[i], 1, TFT_ORANGE, TFT_BLACK);
+  // sprite.drawWedgeLine(sx3, sy3, x3[s.toInt()], y3[s.toInt()], 4, 1, TFT_ORANGE, TFT_BLACK);
+  // sprite.fillSmoothCircle(sx3, sy3, 2, TFT_BLACK, TFT_ORANGE);
 
-  for (int i = 0; i < 60; i++)
-    if (i % 5 == 0)
-      sprite.fillSmoothCircle(px3[i], py3[i], 1, TFT_ORANGE, TFT_BLACK);
-  sprite.drawWedgeLine(sx3, sy3, x3[s.toInt()], y3[s.toInt()], 4, 1, TFT_ORANGE, TFT_BLACK);
-  sprite.fillSmoothCircle(sx3, sy3, 2, TFT_BLACK, TFT_ORANGE);
+  // sprite.unloadFont();
 
-  sprite.unloadFont();
+  // // date #########################################################
+  // sprite.loadFont(smallFont);
+  // sprite.setTextColor(0x35D7, TFT_BLACK);
+  // sprite.drawString(months[month - 1], 22, 84);
+  // sprite.fillRect(12, 94, 22, 30, grays[8]);
+  // sprite.setTextColor(TFT_ORANGE, grays[8]);
+  // sprite.drawString(String(day), 22, 110);
+  // sprite.setTextColor(grays[3], TFT_BLACK);
+  // sprite.drawString(String(sleepTime), sx2, sy2 + 2);
+  // sprite.unloadFont();
 
-  // date #########################################################
-  sprite.loadFont(smallFont);
-  sprite.setTextColor(0x35D7, TFT_BLACK);
-  sprite.drawString(months[month - 1], 22, 84);
-  sprite.fillRect(12, 94, 22, 30, grays[8]);
-  sprite.setTextColor(TFT_ORANGE, grays[8]);
-  sprite.drawString(String(day), 22, 110);
-  sprite.setTextColor(grays[3], TFT_BLACK);
-  sprite.drawString(String(sleepTime), sx2, sy2 + 2);
-  sprite.unloadFont();
-
-  // stopwatch ################
-  sprite.loadFont(middleFont);
-  sprite.setTextColor(TFT_BLACK, grays[4]);
-  sprite.setTextColor(grays[2], TFT_BLACK);
-  sprite.drawString(":", 88 - 12, 186);
-  sprite.drawString(":", 88 + 12, 186);
-  sprite.drawString(secc, 88, 186);
-  sprite.drawString(minn, 88 - 24, 186);
-  sprite.drawString(mill, 88 + 24, 186);
-  sprite.unloadFont();
+ 
 
   // time ##########################
-  sprite.loadFont(bigFont);
-  sprite.setTextColor(grays[0], TFT_BLACK);
-  sprite.drawString(h + ":" + m, 52, 52);
-  sprite.unloadFont();
+  drawTime();
+
   sprite.loadFont(Noto);
 
-  //////brightness region
-  sprite.fillSmoothRoundRect(110, 70, 20, 58, 2, blue, TFT_BLACK);
-  sprite.fillSmoothCircle(sx, sy, 5, grays[1], blue);
-  for (int i = 0; i < 8; i++)
-    sprite.drawWedgeLine(px[i], py[i], x[i], y[i], 1, 1, grays[1], blue);
-  for (int i = 0; i < b + 1; i++)
-    sprite.fillRect(115, 122 - (i * 5), 10, 3, grays[1]);
+  // //////brightness region
+  // sprite.fillSmoothRoundRect(110, 70, 20, 58, 2, blue, TFT_BLACK);
+  // sprite.fillSmoothCircle(sx, sy, 5, grays[1], blue);
+  // for (int i = 0; i < 8; i++)
+  //   sprite.drawWedgeLine(px[i], py[i], x[i], y[i], 1, 1, grays[1], blue);
+  // for (int i = 0; i < b + 1; i++)
+  //   sprite.fillRect(115, 122 - (i * 5), 10, 3, grays[1]);
 
-  for (int i = 0; i < 10; i++)
-    sprite.drawWedgeLine(px2[i], py2[i], x2[i], y2[i], 2, 2, 0x7000, TFT_BLACK);
+  // for (int i = 0; i < 10; i++)
+  //   sprite.drawWedgeLine(px2[i], py2[i], x2[i], y2[i], 2, 2, 0x7000, TFT_BLACK);
 
-  for (int i = 0; i < sleepTime; i++)
-    sprite.drawWedgeLine(px2[i], py2[i], x2[i], y2[i], 2, 2, TFT_RED, TFT_BLACK);
+  // for (int i = 0; i < sleepTime; i++)
+  //   sprite.drawWedgeLine(px2[i], py2[i], x2[i], y2[i], 2, 2, TFT_RED, TFT_BLACK);
 
-  // butons
-  sprite.setTextColor(TFT_BLACK, TFT_ORANGE);
-  sprite.drawString("SET", 22, 225);
-  sprite.setTextColor(grays[1], blue2);
-  sprite.drawString("STA/STP", 87, 220);
-  sprite.setTextColor(grays[3], TFT_BLACK);
+  // // butons
+  // sprite.setTextColor(TFT_BLACK, TFT_ORANGE);
+  // sprite.drawString("SET", 22, 225);
+  // sprite.setTextColor(grays[1], blue2);
+  // sprite.drawString("STA/STP", 87, 220);
+  // sprite.setTextColor(grays[3], TFT_BLACK);
+  sprite.unloadFont();
 
   // batery region
+  drawBattery(200, 4);
+  // // region
 
-  sprite.unloadFont();
-  sprite.setTextColor(grays[5], TFT_BLACK);
-  sprite.drawString(String(vol / 1000.00), 121, 52);
-  sprite.drawRect(114, 12, 14, 28, grays[2]);
-  sprite.fillRect(118, 9, 6, 3, grays[2]);
-  for (int i = 0; i < volE; i++)
-    sprite.fillRect(116, 35 - (i * 5), 10, 3, TFT_GREEN);
+  
+  // // buzzer
+  // sprite.setTextColor(TFT_ORANGE, TFT_BLACK);
+  // sprite.drawString("BEEP", 110, 134);
+  // if (buzzer)
+  //   sprite.drawString("ON", 110, 146);
+  // else
+  //   sprite.drawString("OFF", 110, 146);
 
-  sprite.setTextDatum(0);
-  sprite.setTextColor(grays[4], TFT_BLACK);
-  sprite.drawString("SLEEP", 9, 202);
-  sprite.setTextColor(grays[2], TFT_BLACK);
-  sprite.drawString("STOPWATCH", 55, 165);
-  sprite.drawString("TIME–AND–XXXX", 12, 14);
+  // sprite.setTextColor(grays[6], TFT_BLACK);
+  // sprite.drawString("VOLOS YT", 55, 198);
+  // sprite.setTextColor(grays[6], TFT_BLACK);
+  // sprite.drawString(String(s), 88, 116);
 
-  // buzzer
-  sprite.setTextColor(TFT_ORANGE, TFT_BLACK);
-  sprite.drawString("BEEP", 110, 134);
-  if (buzzer)
-    sprite.drawString("ON", 110, 146);
-  else
-    sprite.drawString("OFF", 110, 146);
+  // sprite.fillTriangle(94, 142, 104, 135, 104, 151, TFT_ORANGE);
+  // sprite.fillRect(94, 139, 8, 9, TFT_ORANGE);
 
-  sprite.setTextColor(grays[6], TFT_BLACK);
-  sprite.drawString("VOLOS YT", 55, 198);
-  sprite.setTextColor(grays[6], TFT_BLACK);
-  sprite.drawString(String(s), 88, 116);
+  // sprite.fillRect(36, 134, 3, 5, TFT_RED);
+  // for (int i = 0; i < 10; i++)
+  //   if (i == poz)
+  //     sprite.fillRect(30 + (i * 6), 142, 3, 6, TFT_RED);
+  //   else
+  //     sprite.fillRect(30 + (i * 6), 142, 3, 6, grays[6]);
 
-  sprite.fillTriangle(94, 142, 104, 135, 104, 151, TFT_ORANGE);
-  sprite.fillRect(94, 139, 8, 9, TFT_ORANGE);
-
-  sprite.fillRect(36, 134, 3, 5, TFT_RED);
-  for (int i = 0; i < 10; i++)
-    if (i == poz)
-      sprite.fillRect(30 + (i * 6), 142, 3, 6, TFT_RED);
-    else
-      sprite.fillRect(30 + (i * 6), 142, 3, 6, grays[6]);
-
-  StickCP2.Display.pushImage(0, 0, 135, 240, (uint16_t *)sprite.getPointer());
+  pushSprite();
 }
+
+void drawTime()
+{
+  int rectX = 6;
+  // int rectWidth = 98;
+  int rectWidth = 120;
+  int midX = (rectX + rectWidth)/2;
+  int border = 2;
+  sprite.fillSmoothRoundRect(rectX, 6, rectWidth, 124, grays[7], TFT_BLACK);
+  sprite.fillSmoothRoundRect(rectX+border, 8, rectWidth-2*border, 120, 2, TFT_BLACK, grays[7]);
+
+  sprite.loadFont(bigFont);
+  sprite.setTextDatum(MC_DATUM);
+
+  sprite.setTextColor(grays[0], TFT_BLACK);
+  int textRectHeight = 52;
+  sprite.drawString(h + ":" + m, midX, textRectHeight);
+  // Flight time
+  
+  int diff = rtc.getEpoch() - timerStartEpoch;
+  String flightTime = formatTime(diff / 3600) + ":" + formatTime(diff / 60 % 60) + ":" + formatTime(diff % 60);
+  sprite.drawString(flightTime, midX, 2*textRectHeight+2*border);
+  sprite.unloadFont();
+
+  
+  sprite.setTextDatum(TC_DATUM);
+  sprite.setTextColor(grays[4], TFT_BLACK);
+  sprite.drawString("TIME", midX, 14);
+  sprite.drawString("FLIGHT TIME", midX, 72);
+}
+
+void pushSprite()
+{
+  StickCP2.Display.setRotation(1);
+  StickCP2.Display.pushImage(0, 0, 240, 135, (uint16_t *)sprite.getPointer());
+  StickCP2.Display.setRotation(0);
+}
+
+void drawBattery(int topX, int topY)
+{
+  // sprite.setTextColor(grays[5], TFT_BLACK);
+  // sprite.drawString(String(vol / 1000.00), topX+18, topY+20);
+  sprite.drawRect(topX+3, topY, 28, 14, grays[2]);
+  sprite.fillRect(topX, topY+4, 3, 6, grays[2]);
+  for (int i = 0; i < volE; i++)
+    sprite.fillRect(topX+26 - (i * 5), topY+2, 3, 10, TFT_GREEN);
+}
+
 
 void drawSet()
 {
-  sprite.fillSprite(TFT_BLACK);
-  sprite.setTextDatum(4);
-  sprite.fillSmoothRoundRect(48, 214, 76, 18, 2, blue2, TFT_BLACK);
-  sprite.fillSmoothRoundRect(6, 214, 34, 18, 2, TFT_ORANGE, TFT_BLACK);
-  sprite.fillSmoothRoundRect(91, 190, 34, 18, 2, TFT_ORANGE, TFT_BLACK);
+  // sprite.fillSprite(TFT_BLACK);
+  sprite.fillSprite(TFT_NAVY);
+  // sprite.setTextDatum(4);
+  // sprite.fillSmoothRoundRect(48, 214, 76, 18, 2, blue2, TFT_BLACK);
+  // sprite.fillSmoothRoundRect(6, 214, 34, 18, 2, TFT_ORANGE, TFT_BLACK);
+  // sprite.fillSmoothRoundRect(91, 190, 34, 18, 2, TFT_ORANGE, TFT_BLACK);
 
-  sprite.loadFont(Noto);
-  sprite.setTextColor(TFT_BLACK, TFT_ORANGE);
-  sprite.drawString("OK", 22, 225);
-  sprite.drawString("CNG", 108, 200);
-  sprite.setTextColor(grays[1], blue2);
-  sprite.drawString("SET", 87, 225);
-  sprite.unloadFont();
+  // sprite.loadFont(Noto);
+  // sprite.setTextColor(TFT_BLACK, TFT_ORANGE);
+  // sprite.drawString("OK", 22, 225);
+  // sprite.drawString("CNG", 108, 200);
+  // sprite.setTextColor(grays[1], blue2);
+  // sprite.drawString("SET", 87, 225);
+  // sprite.unloadFont();
 
-  sprite.loadFont(smallFont);
-  sprite.setTextColor(TFT_ORANGE, TFT_BLACK);
-  sprite.drawString("SETUP", 28, 18);
-  sprite.unloadFont();
+  // sprite.loadFont(smallFont);
+  // sprite.setTextColor(TFT_ORANGE, TFT_BLACK);
+  // sprite.drawString("SETUP", 28, 18);
+  // sprite.unloadFont();
 
-  for (int i = 0; i < 7; i++)
-  {
-    if (chosen == i)
-      tmpColor = grays[5];
-    else
-      tmpColor = grays[9];
+  // for (int i = 0; i < 7; i++)
+  // {
+  //   if (chosen == i)
+  //     tmpColor = grays[5];
+  //   else
+  //     tmpColor = grays[9];
 
-    if (i < 6)
-    {
-      sprite.fillRect(setPosX[i], setPosY[i], 36, 40, tmpColor);
-      sprite.loadFont(smallFont);
-      sprite.setTextColor(0x35D7, TFT_BLACK);
-      sprite.drawString(setDataLbl[i], setPosX[i] + 18, setPosY[i] - 12);
-      sprite.unloadFont();
+  //   if (i < 6)
+  //   {
+  //     sprite.fillRect(setPosX[i], setPosY[i], 36, 40, tmpColor);
+  //     sprite.loadFont(smallFont);
+  //     sprite.setTextColor(0x35D7, TFT_BLACK);
+  //     sprite.drawString(setDataLbl[i], setPosX[i] + 18, setPosY[i] - 12);
+  //     sprite.unloadFont();
 
-      sprite.loadFont(secFont);
-      sprite.setTextColor(grays[1], tmpColor);
-      sprite.drawString(String(setData[i]), setPosX[i] + 17, setPosY[i] + 22);
-      sprite.unloadFont();
-    }
-    else
-    {
-      sprite.setTextDatum(0);
-      sprite.fillRect(setPosX[i], setPosY[i], 76, 16, tmpColor);
-      sprite.setTextColor(grays[1], tmpColor);
-      sprite.drawString(setDataLbl[i], setPosX[i] + 4, setPosY[i] + 5);
-      if (i == 6)
-        if (setData[i] == 0)
-          sprite.drawString("OFF", setPosX[i] + 50, setPosY[i] + 5);
-        else
-          sprite.drawString("ON", setPosX[i] + 50, setPosY[i] + 5);
-    }
-  }
+  //     sprite.loadFont(secFont);
+  //     sprite.setTextColor(grays[1], tmpColor);
+  //     sprite.drawString(String(setData[i]), setPosX[i] + 17, setPosY[i] + 22);
+  //     sprite.unloadFont();
+  //   }
+  //   else
+  //   {
+  //     sprite.setTextDatum(0);
+  //     sprite.fillRect(setPosX[i], setPosY[i], 76, 16, tmpColor);
+  //     sprite.setTextColor(grays[1], tmpColor);
+  //     sprite.drawString(setDataLbl[i], setPosX[i] + 4, setPosY[i] + 5);
+  //     if (i == 6)
+  //       if (setData[i] == 0)
+  //         sprite.drawString("OFF", setPosX[i] + 50, setPosY[i] + 5);
+  //       else
+  //         sprite.drawString("ON", setPosX[i] + 50, setPosY[i] + 5);
+  //   }
+  // }
 
-  StickCP2.Display.pushImage(0, 0, 135, 240, (uint16_t *)sprite.getPointer());
+  pushSprite();
 }
 
 void loop() // ##########################  LOOP  ##################################
 {
   StickCP2.update();
+  if (fase == 0)
+  {
+    timerStartEpoch = rtc.getEpoch();        
+  }
   if (configMode)
   {
     configScreen();    
@@ -418,22 +445,6 @@ void mainScreen()
     sleepTime = 10;
   }
 
-  if (fase == 1)
-  {
-    if (rtc.getSecond() < 10)
-      secc = "0" + String(rtc.getSecond());
-    else
-      secc = String(rtc.getSecond());
-    if (rtc.getMinute() < 10)
-      minn = "0" + String(rtc.getMinute());
-    else
-      minn = String(rtc.getMinute());
-    if (rtc.getMillis() / 10 < 10)
-      mill = "0" + String(rtc.getMillis() / 10);
-    else
-      mill = String(rtc.getMillis() / 10);
-  }
-
   if (StickCP2.BtnB.wasPressed())
   {
     sleepTime = 10;
@@ -459,13 +470,8 @@ void mainScreen()
       if (fase == 3)
         fase = 0;
       if (fase == 1)
-        rtc.setTime(0, 0, 0, 1, 1, 2023, 0);
-      if (fase == 0)
-      {
-        minn = "00";
-        secc = "00";
-        mill = "00";
-      }
+        timerStartEpoch = rtc.getEpoch();
+      
       if (buzzer)
         StickCP2.Speaker.tone(6000, 100);
     }
@@ -504,6 +510,14 @@ void mainScreen()
   }
 
   drawMain();
+}
+
+String formatTime(int time)
+{
+  if (time < 10)
+    return "0" + String(time);
+  else
+    return String(time);
 }
 
 void configScreen()
